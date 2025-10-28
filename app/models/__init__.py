4d993f7880af37726b2c6779706965ff162f2674
Luka Pipi Pilipovic <lukapilip@gmail.com>
2025-10-28 15:15:03 +0100

    mig

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -8,6 +8,17 @@ from .task_log import TaskLog
 from .conversation_state import ConversationState
 from .calendar_integration import CalendarIntegration
 from .availability import AvailabilityRule, AvailabilityOverride
+from .api_key import APIKey  # Add these
+from .api_request_log import APIRequestLog
+from .business_knowledge import BusinessKnowledge
+from .conversation_metrics import ConversationMetrics
+from .user import User  # If you still use these
+from .refresh_token import RefreshToken
+from .email_verification import EmailVerification
+from .invite import Invite
+from .password_reset import PasswordReset
+from .webhook_event import WebhookEvent
+from .webhook_endpoint import WebhookEndpoint
 
 __all__ = [
     "Base",
@@ -22,4 +33,15 @@ __all__ = [
     "CalendarIntegration",
     "AvailabilityRule",
     "AvailabilityOverride",
+    "APIKey",
+    "APIRequestLog",
+    "BusinessKnowledge",
+    "ConversationMetrics",
+    "User",
+    "RefreshToken",
+    "EmailVerification",
+    "Invite",
+    "PasswordReset",
+    "WebhookEvent",
+    "WebhookEndpoint",
 ]
